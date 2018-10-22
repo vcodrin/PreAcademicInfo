29c1db8ef471b2ebf4848bed5edae4f9db5d44fc
CODRINEL <vrabiecodrin@gmail.com>
2018-10-22 19:38:22 +0300

    Added template project

---
+++ src/main/java/com/example/preAcademicInfo/PreAcademicInfoApplication.java
@@ -0,0 +1,12 @@
+package com.example.preAcademicInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreAcademicInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreAcademicInfoApplication.class, args);
+	}
+}
